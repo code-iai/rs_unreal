9fa742d38274adb2075003f11f670ad16c79cdab
robosherlock <robosherlock@ai.uni-bremen.de>
2019-10-11 15:52:54 +0200

    adding kimect to ue4 cordinates mapping

--- rs_ue4beliefstate/src/annotators/UE4Spawner.cpp
+++ rs_ue4beliefstate/src/annotators/UE4Spawner.cpp
@@ -17,7 +17,8 @@
 #include <pcl/surface/convex_hull.h>
 #include <pcl/surface/concave_hull.h>
 #include <pcl/console/time.h>
-
+//ROS
+#include <tf/transform_listener.h>
 //RS
 #include <rs/scene_cas.h>
 #include <rs/utils/time.h>
@@ -33,6 +34,8 @@ class UE4Spawner : public Annotator
 private:
   std::string domain="pie_rwc";
   BeliefStateCommunication* bf_com;
+  tf::TransformListener listener;
+  ros::WallTime start_, end_;
 
 
 public:
@@ -94,15 +97,34 @@ public:
       bf_com->rsToUE4ModelMap(srv);
       //set the ID of the hypothesis to spawn or automatic generation of ID
       //srv.request.id=argv[2];
-
+      tf::StampedTransform transform;
+      //transform from kinect to ue4
+      listener.lookupTransform("/ue4_world","/head_mount_kinect_rgb_link", 
+                               ros::Time(0), transform);
+      geometry_msgs::PoseStamped p,q;
+      p.header.frame_id="/head_mount_kinect_rgb_optical_frame";
+      p.pose.position.x = poses[0].camera.get().translation.get()[0];
+      p.pose.position.y = poses[0].camera.get().translation.get()[1];
+      p.pose.position.z = poses[0].camera.get().translation.get()[2];
+      p.pose.orientation.x = poses[0].camera.get().rotation.get()[0];
+      p.pose.orientation.y = poses[0].camera.get().rotation.get()[1];
+      p.pose.orientation.z = poses[0].camera.get().rotation.get()[2];
+      p.pose.orientation.w = poses[0].camera.get().rotation.get()[3];
+      ROS_ERROR("Hello");
+      try{
+        listener.transformPose("/ue4_world",p,q);
+      }catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+      }
+      ROS_ERROR("Hallo");
       //set the pose of the hypothesis
-      srv.request.pose.position.x = poses[0].world.get().translation.get()[0];
-      srv.request.pose.position.y = poses[0].world.get().translation.get()[1];
-      srv.request.pose.position.z = poses[0].world.get().translation.get()[2];
-      srv.request.pose.orientation.x = poses[0].world.get().rotation.get()[0];
-      srv.request.pose.orientation.y = poses[0].world.get().rotation.get()[1];
-      srv.request.pose.orientation.z = poses[0].world.get().rotation.get()[2];
-      srv.request.pose.orientation.w = poses[0].world.get().rotation.get()[3];
+      srv.request.pose.position.x = q.pose.position.x;
+      srv.request.pose.position.y = q.pose.position.y;
+      srv.request.pose.position.z = q.pose.position.z;
+      srv.request.pose.orientation.x = q.pose.orientation.x;
+      srv.request.pose.orientation.y = q.pose.orientation.y;
+      srv.request.pose.orientation.z = q.pose.orientation.z;
+      srv.request.pose.orientation.w = q.pose.orientation.w;
 
       //set the mobility of the hypothesis
       srv.request.physics_properties.mobility = 0;
